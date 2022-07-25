bd5ce738a7ac8fb919e014d99ad9b57e64f8a674
imamsudarajat04 <imamsudrajat707@gmail.com>
2022-07-25 19:12:27 +0700

    Perbaikkan Navbar

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -19,7 +19,6 @@
             </div>
             <div id="layoutSidenav_content">
                 <main>
-                    
                     @yield('content')
                 </main>
                 @include('layouts.footer')
