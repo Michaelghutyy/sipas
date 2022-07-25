23ee8abe69d8b2b092314da8d4ebd62a6bc91a8c
imamsudarajat04 <imamsudrajat707@gmail.com>
2022-07-25 19:32:48 +0700

    Model dan Table Surat Masuk

---
+++ database/migrations/2022_07_25_122241_create_surat_masuks_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSuratMasuksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('surat_masuks', function (Blueprint $table) {
+            $table->id();
+            $table->string('kodesuratMasuk');
+            $table->string('nosuratMasuk');
+            $table->date('tglSurat');
+            $table->date('tglsuratMasuk');
+            $table->string('asalSurat');
+            $table->string('perihal');
+            $table->string('fileSurat');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('surat_masuks');
+    }
+}
