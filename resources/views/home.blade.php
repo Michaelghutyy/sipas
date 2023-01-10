2fc57cf4726c2e640e0034e185d112de917c8804
michaelghutyy <michaelghut@gmail.com>
2023-01-10 12:55:16 +0700

    Layouting

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -17,7 +17,7 @@
         </div>
 
         <div class="row">
-            <div class="col-xl-3 col-md-6">
+            <div class="col-xl-6 col-md-6">
                 <div class="card bg-primary text-white mb-4">
                     <div class="card-body"><i class="fas fa-mail-bulk"></i> Surat Masuk ({{ $CSM }})</div>
                     <div class="card-footer d-flex align-items-center justify-content-between">
@@ -27,7 +27,7 @@
                 </div>
             </div>
     
-            <div class="col-xl-3 col-md-6">
+            <div class="col-xl-6 col-md-6">
                 <div class="card bg-success text-white mb-4">
                     <div class="card-body"><i class="fas fa-paper-plane"></i> Surat Keluar ({{ $CSK }})</div>
                     <div class="card-footer d-flex align-items-center justify-content-between">
